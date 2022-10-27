32b19ff87592632a49c4257146da9e70adb872de
Anitank <anita.nkwocha18@gmail.com>
2022-10-27 08:18:13 -0700

    lk

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -18,25 +18,13 @@ char *string_toupper(char *p)
 	int a = 0;
 
 	while (p[a])
-
+	{
+		if (p[a] >= 97 && p[a] <= 122)
 		{
-
-			if (p[a] >= 97 && p[a] <= 122)
-
-				{
-
-					p[a] -= 32;
-
-				}
-
-
-
-				a++;
-
-			}
-
-
-
+			p[a] -= 32;
+		}
+		a++;
+	}
 	return (p);
 
 }
