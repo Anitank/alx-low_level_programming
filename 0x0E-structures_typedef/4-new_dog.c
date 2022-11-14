c914e12cd1c2bb7b39ecdce38b3b85125728501c
Anitank <anita.nkwocha18@gmail.com>
2022-11-14 09:49:52 -0800

    tp

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -7,42 +7,23 @@
  * @name: name's dog
  * @age: age's dog
  * @owner: owner's dog
- * Return: newdog
+ * Return: pointer to struc
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
-{
-	int i = 0, j = 0, k;
-	dog_t *doge;
 
-	while (name[i] != '\0')
-		i++;
-	while (owner[j] != '\0')
-		j++;
-	doge = malloc(sizeof(dog_t));
-	if (doge == NULL)
-	{
-		free(doge);
-		return (NULL);
-	}
-	doge->name = malloc(i * sizeof(doge->name));
-	if (doge->name == NULL)
-		free(doge->name);
-	free(doge);
-	return (NULL);
-}
-for (k = 0; k <= i; k++)
-doge->name[k] = name[k];
-doge->age = age;
-doge->owner = malloc(j * sizeof(doge->owner));
-if (doge->owner == NULL)
 {
-	free(doge->owner);
-	free(doge->name);
-	free(doge);
-	return (NULL);
-}
-for (k = 0; k <= j; k++)
-doge->owner[k] = owner[k];
-return (doge);
+
+	struct dog *ret_dog;
+
+	ret_dog = malloc(sizeof(struct dog));
+
+	if (ret_dog == NULL)
+		return (NULL);
+
+	ret_dog->name = name;
+	ret_dog->age = age;
+	ret_dog->owner = owner;
+
+	return (ret_dog);
 }
