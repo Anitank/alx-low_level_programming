ecfe7217d34bc08c56a08ea673fc7f8468b7e38a
Anitank <anita.nkwocha18@gmail.com>
2022-11-29 07:34:52 -0800

    linked

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,16 +9,9 @@
  *
  * @head: head of a list.
  *
- * @idx: index of the list where the new node is
- *
- * added.
- *
- * @n: integer element.
- *
- * Return: the address of the new node, or NULL if it
- *
- * failed.
- *
+ * @idx: index of the list
+ * Return: If the function fails - NULL.
+ * Otherwise - the address of the new node.
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
